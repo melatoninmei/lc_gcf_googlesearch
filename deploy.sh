01a0cf199d4afb103ea7e67997a4be8045ba41d4
Mei Brough-Smyth <hicough@gmail.com>
2023-09-16 10:41:29 +1000

    Extra memory allocation

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 source env.sh
 
 # Check if all the the environment variable are set
-if [ -z "$REGION" ] || [ -z "$GOOGLE_CSE_ID" || -z "$GOOGLE_API_KEY" || -z "OPENAI_API_KEY" ]; then
+if [ -z "$REGION" ] || [ -z "$GOOGLE_CSE_ID" ] || [ -z "$GOOGLE_API_KEY" ] || [ -z "OPENAI_API_KEY" ]; then
   echo "Please set all the environment variables in env.sh"
   exit 1
 fi
@@ -14,5 +14,6 @@ gcloud functions deploy google-search-ai-http \
     --source=. \
     --entry-point=hello_http \
     --trigger-http \
+    --memory=512MB \
     --allow-unauthenticated \
     --set-env-vars GOOGLE_CSE_ID=${GOOGLE_CSE_ID},GOOGLE_API_KEY=${GOOGLE_API_KEY},OPENAI_API_KEY=${OPENAI_API_KEY}
